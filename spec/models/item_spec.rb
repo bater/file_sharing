9a8383d94fafa7b0bec41aec21e275b08501f6f3
bater <jace_ka@yahoo.com.tw>
2022-08-07 23:50:01 +0800

    Add Item#expired_after spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,4 +5,23 @@ RSpec.describe Item, type: :model do
     let(:item) { build(:item, :expired) }
     it { expect(item.expired?).to eq true }
   end
+
+  describe '#expired_after' do
+    subject { build(:item).expired_after }
+    before do
+      allow_any_instance_of(Item).to receive(:remain_seconds).and_return(remain)
+    end
+    describe do
+      let(:remain) { 200 }
+      it { is_expected.to eq '03:20' }
+    end
+    describe do
+      let(:remain) { 60 }
+      it { is_expected.to eq '01:00' }
+    end
+    describe do
+      let(:remain) { 601 }
+      it { is_expected.to eq '10:01' }
+    end
+  end
 end
